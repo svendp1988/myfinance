94ffdf2b48d37644b4afceb48e4320b014e32733
Sven De Potter <sven.depotter@hotmail.com>
2021-02-13 08:01:06 +0100

    Initial commit

---
+++ src/test/java/com/example/myfinance/MyfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
